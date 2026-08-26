f778bcd8aa78529fb2293bd4d575528511860dbe
Subham Negi <negisubham210@gmail.com>
2026-08-26 12:23:30 +0530

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Number
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 10;
+let b = 20;
+// String
+let str = "Rahul";
+// Boolean
+let isTrue = true;
+// Bigint
+let bigIntNum = 9007199254740991n;
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,20 @@
+
+// Number
+
+let a:number = 10
+
+let b:number = 20
+
+
+
+// String
+
+let str:string = "Rahul"
+
+// Boolean
+
+let isTrue:boolean = true
+
+// Bigint
+
+let bigIntNum:bigint = 9007199254740991n

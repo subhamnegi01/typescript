1ae4e2c5732bba94ee7f88d785160d4b61f92e72
Subham Negi <negisubham210@gmail.com>
2026-08-26 22:26:59 +0530

    update

---
+++ 02.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = 10;
+let x = 20;
+let val;
+val = 10;
+let val2;
+val2 = "Rahul";
+console.log(val);
+console.log(val2.toUpperCase());
+if (typeof val2 === "string") {
+    console.log(val2.toUpperCase());
+}
+//# sourceMappingURL=02.js.map
---
+++ 02.ts
@@ -0,0 +1,17 @@
+let num:number = 10
+
+let x = 20
+
+let val:unknown
+val = 10
+
+let val2:any;
+val2 = "Rahul"
+
+
+console.log(val)
+console.log(val2.toUpperCase())
+
+if(typeof val2 === "string") {
+    console.log(val2.toUpperCase())
+}

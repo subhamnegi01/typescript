268903c7db98ed7b42318e243ae6f7a4f8e05a02
Subham Negi <negisubham210@gmail.com>
2026-08-28 11:36:16 +0530

    update

--- 03/first.js
+++ 03/first.js
@@ -14,4 +14,35 @@ const obj3 = {
     age: 25,
     balance: 1000
 };
+const obj4 = {
+    name: "Smith",
+    age: 25,
+    balance: 1000
+};
+const arr = [{ name: "Rohit", age: 30, gender: "Male" }, { name: "mohit", age: 25, gender: "Male" }];
+// function in TS
+function greet(a) {
+    console.log(a);
+    return a + 5;
+}
+console.log(greet(5));
+function meet(msg, val) {
+    console.log(msg, val);
+}
+meet("Himanshu", 4);
+function neet(msg = "JIT") {
+    console.log(msg);
+}
+neet();
+// Optional Parameter
+function GATE(person) {
+    console.log(person || "Rohan");
+}
+GATE();
+GATE("Subham");
+// Arrow fun
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(3, 4));
 //# sourceMappingURL=first.js.map
--- 03/first.ts
+++ 03/first.ts
@@ -34,3 +34,55 @@ const obj4: Readonly<customer> = {
     age: 25,
     balance: 1000
 }
+
+
+
+// Array of objects
+
+interface people{name: string, age: number, gender: string}
+
+const arr: people[] = [{name: "Rohit", age: 30, gender: "Male"}, {name: "mohit", age: 25, gender: "Male"}]
+
+
+
+// function in TS
+
+function greet (a:number):number{
+    console.log(a)
+    return a+5
+}
+console.log(greet(5))
+
+
+function meet (msg:string, val:number):void{
+    console.log(msg,val)
+}
+
+meet("Himanshu", 4)
+
+
+function neet(msg:string = "JIT"){
+    console.log(msg)
+}
+
+neet()
+
+
+
+// Optional Parameter
+
+function GATE(person?:string){
+    console.log(person|| "Rohan")
+}
+
+GATE()
+GATE("Subham")
+
+
+
+// Arrow fun
+
+const sum=(a:number,b:number):number=>{
+    return a+b
+}
+console.log(sum(3,4))
